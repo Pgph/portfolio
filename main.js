c194001381d5131b91aea800acd0edd2cb8b1a64
Patryk Borkowski <patrykborkowski.com@gmail.com>
2022-11-27 23:13:30 +0100

    Added finishes transition

--- main.js
+++ main.js
@@ -34,6 +34,7 @@ slideContainer.addEventListener("sliderMove", () => {
   // 3. renable disabled attribute if needed
   currentIndex === 0 && addDisabledAttribute([slideBtns[0]]);
 });
+
 // transition end event
 slideContainer.addEventListener("transitionend", () => (isMoving = false));
 
@@ -54,31 +55,32 @@ const slideObserver = new IntersectionObserver(
 
 slideObserver.observe(slides[slides.length - 1]);
 
-//form handle
-
+// FORM HANDLE //
 const contactForm = document.querySelector("#contact-form");
 const contactBtn = document.querySelector("#contact-btn");
 const contactInput = document.querySelector("#email");
 
-//fake sending email to api endpoint
+// fake sending email to api endpoint
 function postEmailToDatabase(email) {
+  console.info(`Your email is ${email}`);
   return new Promise((resolve) => setTimeout(resolve, 2000));
 }
 
-//options for submit button
+// options for submit button
 const contactBtnOptions = {
   pending: `
-  <svg class="animate-spin" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="32" x2="128" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="224" y1="128" x2="192" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="195.9" y1="195.9" x2="173.3" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="128" y1="224" x2="128" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="195.9" x2="82.7" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="32" y1="128" x2="64" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="60.1" x2="82.7" y2="82.7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>
-  <span class="uppercase tracking-wide animate-pulse">
+    <svg xmlns="http://www.w3.org/2000/svg" class="animate-spin" width="24" height="24" fill="currentColor" viewBox="0 0 256 256"><rect width="256" height="256" fill="none"></rect><line x1="128" y1="32" x2="128" y2="64" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="224" y1="128" x2="192" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="195.9" y1="195.9" x2="173.3" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="128" y1="224" x2="128" y2="192" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="195.9" x2="82.7" y2="173.3" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="32" y1="128" x2="64" y2="128" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line><line x1="60.1" y1="60.1" x2="82.7" y2="82.7" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="24"></line></svg>
+    <span class="uppercase tracking-wide animate-pulse">
     Sending...
-  </span>
+    </span>
   `,
-  success: `  <span class="uppercase tracking-wide">
-  Thank you!
-</span>
-<span class="uppercase tracking-wide">
-✌️
-</span>`,
+  success: `
+  <span class="uppercase tracking-wide">
+    Thank you!
+    </span>
+    <span class="uppercase tracking-wide">
+    ✌️
+    </span>`,
 };
 
 async function handleFormSubmit(e) {
@@ -91,5 +93,35 @@ async function handleFormSubmit(e) {
   contactBtn.innerHTML = contactBtnOptions.success;
 }
 
-//event listener form
+// event listener form submit
 contactForm.addEventListener("submit", handleFormSubmit);
+
+// FADE UP OBSERVER //
+function fadeUpObserverCallback(elsToWatch) {
+  elsToWatch.forEach((el) => {
+    if (el.isIntersecting) {
+      el.target.classList.add("faded");
+      fadeUpObserver.unobserve(el.target);
+      el.target.addEventListener(
+        "transitionend",
+        () => {
+          el.target.classList.remove("fade-up", "faded");
+        },
+        { once: true }
+      );
+    }
+  });
+}
+
+const fadeUpObserverOptions = {
+  threshold: 0.6,
+};
+
+const fadeUpObserver = new IntersectionObserver(
+  fadeUpObserverCallback,
+  fadeUpObserverOptions
+);
+
+document.querySelectorAll(".fade-up").forEach((item) => {
+  fadeUpObserver.observe(item);
+});
